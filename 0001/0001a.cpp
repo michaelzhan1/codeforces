c6d64bc7867fd7a327b25c1d7aed5033f47b6f78
Michael Zhan <michaelzhan2001@gmail.com>
2025-10-12 22:56:00 -0400

    1a

--- 0001/0001a.cpp
+++ 0001/0001a.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
-#include <string>
+#include <cmath>
+
+long long ceilDiv(long long a, long long b)
+{
+    if (a % b == 0)
+    {
+        return a / b;
+    }
+    else
+    {
+        return ceil(a / float(b));
+    }
+}
 
 int main()
 {
+    long long n, m, a;
+    std::cin >> n >> m >> a;
+    std::cout << ceilDiv(n, a) * ceilDiv(m, a) << std::endl;
     return 0;
 }
