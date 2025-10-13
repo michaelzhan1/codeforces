9699fa9c74a0fd05257ccedb268a0dbe64a046c4
Michael Zhan <michaelzhan2001@gmail.com>
2025-10-12 22:54:08 -0400

    update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@
 
 # ./run.sh {number} {letter}
 
-g++ -Wall -Wpedantic -Wextra -Werror -Wmissing-include-dirs $1/$1$2.cpp
-./a.out < $1/$1$2.txt
+padded=$(printf "%04d" "$1")
+
+g++ -Wall -Wpedantic -Wextra -Werror -Wmissing-include-dirs $padded/$padded$2.cpp
+./a.out < $padded/$padded$2.txt
