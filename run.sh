e2ce79bf0b93d3c20ab67ce14e43066da94c7a20
Michael Zhan <michaelzhan2001@gmail.com>
2025-10-14 21:08:00 -0400

    add run for python

--- run.sh
+++ run.sh
@@ -4,5 +4,11 @@
 
 padded=$(printf "%04d" "$1")
 
-g++ -Wall -Wpedantic -Wextra -Werror -Wmissing-include-dirs $padded/$padded$2.cpp
-./a.out < $padded/$padded$2.txt
+if [ -f "$padded/$padded$2.cpp" ]; then
+    g++ -Wall -Wpedantic -Wextra -Werror -Wmissing-include-dirs $padded/$padded$2.cpp
+    ./a.out < $padded/$padded$2.txt
+fi
+
+if [ -f "$padded/$padded$2.py" ]; then
+    python3 $padded/$padded$2.py < $padded/$padded$2.txt
+fi
