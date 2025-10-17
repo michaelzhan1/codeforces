db0c90b1f938168944ae0666bc3d3f4a0052181d
Michael Zhan <michaelzhan2001@gmail.com>
2025-10-17 01:43:17 -0400

    optimize python solution

--- 0002/0002b.py
+++ 0002/0002b.py
@@ -25,8 +25,6 @@ def main():
 
     dp2 = [[None] * n for _ in range(n)]
     dp5 = [[None] * n for _ in range(n)]
-    dir2 = [[""] * n for _ in range(n)]
-    dir5 = [[""] * n for _ in range(n)]
     factors = count_factors(arr[-1][-1])
     dp2[-1][-1] = factors
     dp5[-1][-1] = factors
@@ -35,14 +33,10 @@ def main():
         factors_i = count_factors(arr[i][-1])
         dp2[i][-1] = add(factors_i, dp2[i + 1][-1])
         dp5[i][-1] = add(factors_i, dp5[i + 1][-1])
-        dir2[i][-1] = "D" + dir2[i + 1][-1]
-        dir5[i][-1] = "D" + dir5[i + 1][-1]
 
         factors_j = count_factors(arr[-1][i])
         dp2[-1][i] = add(factors_j, dp2[-1][i + 1])
         dp5[-1][i] = add(factors_j, dp5[-1][i + 1])
-        dir2[-1][i] = "R" + dir2[-1][i + 1]
-        dir5[-1][i] = "R" + dir5[-1][i + 1]
 
     for i in range(n - 2, -1, -1):
         for j in range(n - 2, -1, -1):
@@ -51,27 +45,46 @@ def main():
             # dp2 takes minimum 2 route
             if dp2[i + 1][j][0] < dp2[i][j + 1][0]:
                 dp2[i][j] = add(factors, dp2[i + 1][j])
-                dir2[i][j] = "D" + dir2[i + 1][j]
             else:
                 dp2[i][j] = add(factors, dp2[i][j + 1])
-                dir2[i][j] = "R" + dir2[i][j + 1]
             
             # dp5 takes minimum 5 route
             if dp5[i + 1][j][1] < dp5[i][j + 1][1]:
                 dp5[i][j] = add(factors, dp5[i + 1][j])
-                dir5[i][j] = "D" + dir5[i + 1][j]
             else:
                 dp5[i][j] = add(factors, dp5[i][j + 1])
-                dir5[i][j] = "R" + dir5[i][j + 1]
     
     min2 = min(dp2[0][0])
     min5 = min(dp5[0][0])
     if min2 < min5:
         print(min2)
-        print(dir2[0][0])
+        dp = dp2
+        idx = 0
     else:
         print(min5)
-        print(dir5[0][0])
+        dp = dp5
+        idx = 1
+
+    
+    i = 0
+    j = 0
+    path = ""
+    while i < n - 1 and j < n - 1:
+        if dp[i + 1][j][idx] < dp[i][j + 1][idx]:
+            path += "D"
+            i += 1
+        else:
+            path += "R"
+            j += 1
+    
+    if i < n:
+        path += "D" * (n - i - 1)
+    if j < n:
+        path += "R" * (n - j - 1)
+    print(path)
+        
+        
+
 
             
 
