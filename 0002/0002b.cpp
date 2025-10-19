c9be3a8e73dbe56d40711083587a4fbc40e1439b
Michael Zhan <michaelzhan2001@gmail.com>
2025-10-19 00:53:48 -0400

    2b

--- 0002/0002b.cpp
+++ 0002/0002b.cpp
@@ -10,7 +10,7 @@ int main()
     int n;
     std::cin >> n;
 
-    int zi, zj;
+    int zi, zj = -1;
     bool has_zero = false;
 
     vvi f1(n, vi(n));
@@ -25,12 +25,12 @@ int main()
             int x1, x2;
             std::cin >> x1;
             x2 = x1;
-            while (x1 % 2 == 0)
+            while (x1 > 0 && x1 % 2 == 0)
             {
                 x1 /= 2;
                 f1[i][j]++;
             }
-            while (x2 % 5 == 0)
+            while (x2 > 0 && x2 % 5 == 0)
             {
                 x2 /= 5;
                 f2[i][j]++;
@@ -47,18 +47,91 @@ int main()
         }
     }
 
-    dp1[0][0] = f1[0][0];
-    dp2[0][0] = f2[0][0];
-    for (int i = 1; i < n; i++) {
-        
+    dp1[n - 1][n - 1] = f1[n - 1][n - 1];
+    dp2[n - 1][n - 1] = f2[n - 1][n - 1];
+    for (int i = n - 2; i >= 0; --i)
+    {
+        dp1[i][n - 1] = dp1[i + 1][n - 1] == -1 ? -1 : f1[i][n - 1] + dp1[i + 1][n - 1];
+        dp2[i][n - 1] = dp2[i + 1][n - 1] == -1 ? -1 : f2[i][n - 1] + dp2[i + 1][n - 1];
+
+        dp1[n - 1][i] = dp1[n - 1][i + 1] == -1 ? -1 : f1[n - 1][i] + dp1[n - 1][i + 1];
+        dp2[n - 1][i] = dp2[n - 1][i + 1] == -1 ? -1 : f2[n - 1][i] + dp2[n - 1][i + 1];
     }
 
-    for (int i = n - 1; i >= 0; --i)
+    for (int i = n - 2; i >= 0; --i)
     {
-        for (int j = n - 1; j >= 0; --j)
+        for (int j = n - 2; j >= 0; --j)
         {
+            if (f1[i][j] == -1)
+                dp1[i][j] = -1;
+            else if (dp1[i + 1][j] == -1 && dp1[i][j + 1] == -1)
+                dp1[i][j] = -1;
+            else if (dp1[i + 1][j] == -1)
+                dp1[i][j] = f1[i][j] + dp1[i][j + 1];
+            else if (dp1[i][j + 1] == -1)
+                dp1[i][j] = f1[i][j] + dp1[i + 1][j];
+            else
+                dp1[i][j] = f1[i][j] + std::min(dp1[i + 1][j], dp1[i][j + 1]);
+
+            if (f2[i][j] == -1)
+                dp2[i][j] = -1;
+            else if (dp2[i + 1][j] == -1 && dp2[i][j + 1] == -1)
+                dp2[i][j] = -1;
+            else if (dp2[i + 1][j] == -1)
+                dp2[i][j] = f2[i][j] + dp2[i][j + 1];
+            else if (dp2[i][j + 1] == -1)
+                dp2[i][j] = f2[i][j] + dp2[i + 1][j];
+            else
+                dp2[i][j] = f2[i][j] + std::min(dp2[i + 1][j], dp2[i][j + 1]);
         }
     }
+
+    // if there's a zero and it's worth taking, path to (zi, zj) directly
+    if (has_zero && std::min(dp1[0][0], dp2[0][0]) >= 1) {
+        std::cout << 1 << std::endl;
+        for (int i = 0; i < zi; ++i)
+            std::cout << 'D';
+        for (int j = 0; j < zj; ++j)
+            std::cout << 'R';
+        for (int i = zi; i < n - 1; ++i)
+            std::cout << 'D';
+        for (int j = zj; j < n - 1; ++j)
+            std::cout << 'R';
+        std::cout << std::endl;
+        return 0;
+    }
+
+    // else, it's better to not take the zero (or there is no zero)
+    vvi dp = dp1[0][0] < dp2[0][0] ? dp1 : dp2;
+    std::cout << dp[0][0] << std::endl;
+    int i = 0, j = 0;
+    while (i < n - 1 && j < n - 1) {
+        if (dp[i + 1][j] == -1) {
+            std::cout << 'R';
+            j++;
+        } else if (dp[i][j + 1] == -1) {
+            std::cout << 'D';
+            i++;
+        } else if (dp[i + 1][j] < dp[i][j + 1]) {
+            std::cout << 'D';
+            i++;
+        } else {
+            std::cout << 'R';
+            j++;
+        }
+    }
+
+    while (i < n - 1) {
+        std::cout << 'D';
+        i++;
+    }
+
+    while (j < n - 1) {
+        std::cout << 'R';
+        j++;
+    }
+
+    std::cout << std::endl;
 
     return 0;
 }
