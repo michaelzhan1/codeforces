effc039cd8a4cb7cc96ed66a8c9c2ed5eb9a5323
Michael Zhan <michaelzhan2001@gmail.com>
2025-10-19 23:28:04 -0400

    3b

--- 0003/0003b.cpp
+++ 0003/0003b.cpp
@@ -58,14 +58,6 @@ int main()
     std::sort(ones.begin(), ones.end());
     std::sort(twos.begin(), twos.end());
 
-    for (auto p : ones)
-    {
-        std::cout << p.first << ' ' << p.second << std::endl;
-    }
-    std::cout << std::endl;
-    for (auto p : twos)
-        std::cout << p.first << ' ' << p.second << std::endl;
-
     int n1 = int(ones.size());
     int n2 = int(twos.size());
 
@@ -75,81 +67,57 @@ int main()
     ll res = 0;
     std::vector<size_t> idxs;
 
+    if (v % 2 == 1 && i >= 0)
+    {
+        size_t ii = size_t(i);
+        res += ones[ii].first;
+        idxs.push_back(ones[ii].second);
+        i--;
+        v--;
+    }
+
+    if (v == 1 && i < 0)
+    {
+        std::cout << 0 << std::endl;
+        return 0;
+    }
+
     // while we still have space and we still have something to choose from
     while (v > 0 && (i >= 0 || j >= 0))
     {
-        size_t i_size_t = size_t(i);
-        size_t j_size_t = size_t(j);
-        // only one space left, must be kayak or nothing
-        if (v == 1)
-        {
-            if (i >= 0)
-            {
-                res += ones[i_size_t].first;
-                idxs.push_back(ones[i_size_t].second);
-            }
-            break;
-        }
-
-        // // three spaces left, and at least 2 kayaks and at least 1 catamaran
-        // if (v == 3 && i >= 1 && j >= 0)
-        // {
-        //     std::cout << i << ' ' << j << std::endl;
-        //     // if >= 3 kayaks and better, take all kayaks
-        //     if (i >= 2 && ones[i_size_t].first + ones[i_size_t - 1].first + ones[i_size_t - 2].first > twos[j_size_t].first)
-        //     {
-        //         res += ones[i_size_t].first + ones[i_size_t - 1].first + ones[i_size_t - 2].first;
-        //         idxs.push_back(ones[i_size_t].second);
-        //         idxs.push_back(ones[i_size_t - 1].second);
-        //         idxs.push_back(ones[i_size_t - 2].second);
-        //     }
-        //     // else if it's worth taking both kayaks, do that
-        //     else if (ones[i_size_t - 1].first > twos[j_size_t].first)
-        //     {
-        //         res += ones[i_size_t].first + ones[i_size_t - 1].first;
-        //         idxs.push_back(ones[i_size_t].second);
-        //         idxs.push_back(ones[i_size_t - 1].second);
-        //     }
-        //     // else, take catamaran and kayak
-        //     else
-        //     {
-        //         res += ones[i_size_t].first + twos[j_size_t].first;
-        //         idxs.push_back(ones[i_size_t].second);
-        //         idxs.push_back(twos[j_size_t].second);
-        //     }
-        //     break;
-        // }
+        size_t ii = size_t(i);
+        size_t jj = size_t(j);
 
         // if only twos are left, take twos
         if (i < 0)
         {
-            res += twos[j_size_t].first;
-            idxs.push_back(twos[j_size_t].second);
+            res += twos[jj].first;
+            idxs.push_back(twos[jj].second);
             j--;
             v -= 2;
         }
         // else if only ones are left, takes ones
         else if (j < 0)
         {
-            res += ones[i_size_t].first;
-            idxs.push_back(ones[i_size_t].second);
+            res += ones[ii].first;
+            idxs.push_back(ones[ii].second);
             i--;
             v--;
         }
-        // else if only 1x one is left, compare
+        // else if only 1x one is left but it's good, compare
         else if (i == 0)
         {
-            if (ones[i_size_t].first > twos[j_size_t].first)
+            if (ones[ii].first > twos[jj].first)
             {
-                res += ones[i_size_t].first;
-                idxs.push_back(ones[i_size_t].second);
+                res += ones[ii].first;
+                idxs.push_back(ones[ii].second);
                 i--;
                 v--;
             }
             else
             {
-                res += twos[j_size_t].first;
-                idxs.push_back(twos[j_size_t].second);
+                res += twos[jj].first;
+                idxs.push_back(twos[jj].second);
                 j--;
                 v -= 2;
             }
@@ -157,18 +125,18 @@ int main()
         // else both have full options, compare
         else
         {
-            if (ones[i_size_t].first + ones[i_size_t - 1].first > twos[j_size_t].first)
+            if (ones[ii].first + ones[ii - 1].first > twos[jj].first)
             {
-                res += ones[i_size_t].first + ones[i_size_t - 1].first;
-                idxs.push_back(ones[i_size_t].second);
-                idxs.push_back(ones[i_size_t - 1].second);
+                res += ones[ii].first + ones[ii - 1].first;
+                idxs.push_back(ones[ii].second);
+                idxs.push_back(ones[ii - 1].second);
                 i -= 2;
                 v -= 2;
             }
             else
             {
-                res += twos[j_size_t].first;
-                idxs.push_back(twos[j_size_t].second);
+                res += twos[jj].first;
+                idxs.push_back(twos[jj].second);
                 j--;
                 v -= 2;
             }
