2e5ccfbd6b6dc78e3b4f8d0829f5c9936f63c996
Michael Zhan <michaelzhan2001@gmail.com>
2025-11-07 18:22:28 -0500

    try 7d again

--- 0007/0007d.cpp
+++ 0007/0007d.cpp
@@ -1,24 +1,10 @@
 #include <algorithm>
-#include <cmath>
 #include <iostream>
-#include <numeric>
-#include <queue>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
 
 // nums
 using ll = long long;
-
-// vectors
-using vi = std::vector<int>;
-using vvi = std::vector<vi>;
-using vs = std::vector<size_t>;
-
-// sets
-using usi = std::unordered_set<int>;
-using uss = std::unordered_set<size_t>;
+using ull = unsigned long long;
 
 // pairs
 template <typename T1, typename T2>
@@ -42,19 +28,18 @@ struct pair_hash
     }
 };
 
-const ll MOD = 998244353;
-const ll BASE = 233;
+// const ll MOD = 998244353;
+constexpr ull BASE = 179ULL;
 
 const int MAX_N = 5000005;
-int dp[MAX_N];
 
-ll pows[MAX_N];
-ll fwd_hsh[MAX_N];
-ll rev_hsh[MAX_N];
+ull dp[MAX_N];
+ull pows[MAX_N];
+ull fwd_hsh[MAX_N];
+ull rev_hsh[MAX_N];
 
-inline ll hash(ll hsh[], size_t l, size_t r)
-{
-    return (hsh[r] - hsh[l - 1] * pows[r - l + 1] % MOD + MOD) % MOD;
+inline ull get_hash(const ull* __restrict h, const ull* __restrict pw, size_t l, size_t r) {
+    return h[r] - h[l - 1] * pw[r - l + 1];
 }
 
 int main()
@@ -65,25 +50,25 @@ int main()
     std::string rev_s = s;
     std::reverse(rev_s.begin(), rev_s.end());
 
-    size_t n = s.size();
+    size_t n = s.size();    
 
     pows[0] = 1;
     fwd_hsh[0] = 0;
     rev_hsh[0] = 0;
 
     for (size_t i = 1; i < n + 1; i++)
-        pows[i] = pows[i - 1] * BASE % MOD;
+        pows[i] = pows[i - 1] * BASE;
     for (size_t i = 1; i < n + 1; i++)
-        fwd_hsh[i] = (fwd_hsh[i - 1] * BASE % MOD + s[i - 1]) % MOD;
+        fwd_hsh[i] = fwd_hsh[i - 1] * BASE + (unsigned char)s[i - 1];
     for (size_t i = 1; i < n + 1; i++)
-        rev_hsh[i] = (rev_hsh[i - 1] * BASE % MOD + rev_s[i - 1]) % MOD;
+        rev_hsh[i] = rev_hsh[i - 1] * BASE + (unsigned char)rev_s[i - 1];
 
     dp[1] = 1;
-    ll res = 1;
+    ull res = 1;
     for (size_t i = 2; i < n + 1; i++)
     {
-        if (hash(fwd_hsh, 1, i / 2) == hash(rev_hsh, n - i + 1, n - i + i / 2))
-            dp[i] = dp[i / 2] + 1;
+        if (get_hash(fwd_hsh, pows, 1, i >> 1) == get_hash(rev_hsh, pows, n - i + 1, n - i + (i >> 1)))
+            dp[i] = dp[i >> 1] + 1;
         res += dp[i];
     }
 
