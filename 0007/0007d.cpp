bbdd9c2e671fc446a1db4d0bdb931ec7e95f7efe
Michael Zhan <michaelzhan2001@gmail.com>
2025-11-08 20:01:47 -0500

    clean up 7d

--- 0007/0007d.cpp
+++ 0007/0007d.cpp
@@ -1,37 +1,51 @@
-// TODO: clean and clarify
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
 
-#include<bits/stdc++.h>
 #define pf printf
 #define sc(x) scanf("%d", &x)
 #define scs(x) scanf("%s", x)
 #define scl(x) scanf("%lld", &x)
-#define mst(a,x) memset(a, x, sizeof(a))
-#define rep(i,s,e) for(int i=s; i<e; ++i)
-#define dep(i,e,s) for(int i=e; i>=s; --i)
-using namespace std;
-typedef long long ll;
-typedef pair<int,int> pii;
-const int maxn = 5e6 + 5;
-char s[maxn],ma[2*maxn];
-int che[2*maxn],ww[2*maxn];
-void manacher(char* s,int len0){
-    int len=0; ma[len++]='$'; ma[len++]='#';
-    rep(i,0,len0) ma[len++]=s[i],ma[len++]='#';
-    ma[len]=0; int maxx=0,num=0;
-    rep(i,0,len){
-        che[i]=maxx>i?min(che[2*num-i],maxx-i):1;
-        while(ma[i+che[i]]==ma[i-che[i]]) che[i]++;
-        if(i+che[i]>maxx) maxx=i+che[i],num=i;
+#define forinc(i, s, e) for (int i = s; i < e; ++i)
+
+using ll = long long;
+
+const int MAXN = 5'000'005;
+char s[MAXN], ma[2 * MAXN];
+int rad[2 * MAXN], dp[2 * MAXN];
+void manacher(char *s, int len0)
+{
+    int len = 0;
+    ma[len++] = '$';
+    ma[len++] = '#';
+    forinc(i, 0, len0) ma[len++] = s[i], ma[len++] = '#';
+    ma[len] = 0;
+    int r = 0, center = 0;
+    forinc(i, 0, len)
+    {
+        rad[i] = i < r ? std::min(rad[2 * center - i], r - i) : 1;
+        while (ma[i + rad[i]] == ma[i - rad[i]])
+            rad[i]++;
+        if (i + rad[i] > r)
+            r = i + rad[i], center = i;
     }
 }
-void solve(int len){
-    ll ans(0); rep(i,1,len+1){
-        int l=2,r=2*i,m=l+r>>1;
-        if(che[m]*2>r-l+1) ww[r]=ww[m-1-(m-1)%2]+1;
-        ans+=ww[r];
-    } pf("%lld\n",ans);
+void solve(int len)
+{
+    ll ans(0);
+    forinc(i, 1, len + 1)
+    {
+        int l = 2, r = 2 * i, m = (l + r) >> 1;
+        if (rad[m] * 2 > r - l + 1)
+            dp[r] = dp[m - 1 - (m - 1) % 2] + 1;
+        ans += dp[r];
+    }
+    pf("%lld\n", ans);
 }
-int main(){
-    scs(s); int len=strlen(s);
-    manacher(s,len); solve(len);
+int main()
+{
+    scs(s);
+    int len = (int)std::strlen(s);
+    manacher(s, len);
+    solve(len);
 }
