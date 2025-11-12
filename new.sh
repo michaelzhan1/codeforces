d4716612e7fd17ada19c11226321b8d4ccf7e7b9
Michael Zhan <michaelzhan2001@gmail.com>
2025-11-12 18:40:12 -0500

    update new

--- new.sh
+++ new.sh
@@ -31,51 +31,17 @@ touch "$txt"
 case "$ext" in
     cpp)
         cat > "$src" << EOF
-#include <algorithm>
-#include <cmath>
-#include <iostream>
-#include <numeric>
-#include <queue>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
+#include <cstdio>
+#include <cstring>
 
-// nums
+#define pf printf
+#define sc(x) scanf("%d", &x)
+#define scs(x) scanf("%s", x)
+#define scl(x) scanf("%lld", &x)
+#define forinc(i, s, e) for (int i = s; i < e; ++i)
+#define fordec(i, e, s) for (int i = e; i >= s; --i)
+
 using ll = long long;
-
-// vectors
-using vi = std::vector<int>;
-using vvi = std::vector<vi>;
-using vs = std::vector<size_t>;
-
-// sets
-using usi = std::unordered_set<int>;
-using uss = std::unordered_set<size_t>;
-
-// pairs
-template <typename T1, typename T2>
-inline std::pair<T1, T2> mp(T1 a, T2 b)
-{
-    return std::make_pair(a, b);
-}
-
-// pair hashing
-struct pair_hash
-{
-    template <class T1, class T2>
-    std::size_t operator()(const std::pair<T1, T2> &p) const
-    {
-        auto h1 = std::hash<T1>{}(p.first);
-        auto h2 = std::hash<T2>{}(p.second);
-
-        // Mainly for demonstration purposes, i.e. works but is overly simple
-        // In the real world, use sth. like boost.hash_combine
-        return h1 ^ h2;
-    }
-};
-
-// problem specific
 
 int main()
 {
